cfb242ac4a63aa963818217842e25ae636935594
Brian Manning <brian@xaoc.org>
2012-02-09 08:53:49 -0800

    added CGI.pm link; reformatted aux links

--- mod_perl-mod_perl2_users_guide.sh
+++ mod_perl-mod_perl2_users_guide.sh
@@ -5,5 +5,7 @@ source examples_lib.sh
 BOOK_NAME="mod_perl2 User's Guide"
 BOOK_ISBN="9780977920112"
 EXAMPLES_URL=""
-AUX_URLS="http://onyxneon.com/books/modperl2/index.html http://modperl2book.org/"
+AUX_URLS="
+- http://onyxneon.com/books/modperl2/index.html
+- http://modperl2book.org/"
 display_info
---
+++ perl-cgi.pm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+source examples_lib.sh
+
+BOOK_NAME="Official Guide to Programming with CGI.pm"
+BOOK_ISBN="0471247448"
+EXAMPLES_URL=""
+AUX_URLS="
+- http://www.wiley.com/legacy/compbooks/stein/index.html
+- http://www.wiley.com/legacy/compbooks/stein/source.html"
+display_info
