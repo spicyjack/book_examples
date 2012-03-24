8c9922d9ed4353dc9760593c930e55ae1dfe1af6
Brian Manning <brian@xaoc.org>
2012-03-23 23:53:13 -0700

    added new book

---
+++ javascript_and_jquery_the_missing_manual.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+source examples_lib.sh
+
+BOOK_NAME="JavaScript and jQuery: The Missing Manual"
+BOOK_ISBN="0636920015048"
+EXAMPLES_URL="http://examples.oreilly.com/0636920015048/MM_JAVASCRIPT2E.zip"
+AUX_URLS="
+- http://sawmac.com/js2e/
+"
+display_info
