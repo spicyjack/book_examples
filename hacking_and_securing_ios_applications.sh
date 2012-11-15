67b8395dc122205cdf7a974f40c2ed85f3d7cd37
Brian Manning <brian@xaoc.org>
2012-11-15 10:15:46 -0800

    added downloader for hacking and securing ios apps

---
+++ hacking_and_securing_ios_applications.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+source examples_lib.sh
+
+BOOK_NAME="Hacking and Securing iOS Applications"
+BOOK_ISBN="1449318746"
+EXAMPLES_URL="http://examples.oreilly.com/0636920023234/All_Source_Code.zip"
+AUX_URLS="
+- http://shop.oreilly.com/product/0636920023234.do
+"
+display_info
