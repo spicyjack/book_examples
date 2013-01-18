756a0c5b1d55cdcbb46119345ce273cae1f50e68
Brian Manning <brian@xaoc.org>
2013-01-17 23:47:01 -0800

    added a new book example downloader

---
+++ javascript_the_definitive_guide.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+source examples_lib.sh
+
+BOOK_NAME="JavaScript: The Definitive Guide"
+BOOK_ISBN="9780596805531"
+EXAMPLES_URL="http://examples.oreilly.com/9780596805531/examples.tar.gz"
+AUX_URLS="
+"
+display_info
